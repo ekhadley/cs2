5ff5d2c49b8f209a749e57b7d38f21c841db2ab9
Ethan Hadley <ekhadley@gmail.com>
2022-01-19 11:55:56 -0600

    bcvncvb

--- lab1.java
+++ lab1.java
@@ -1,54 +1,80 @@
-
-
-
-class lab1{
+public class lab1 {
     public static void main(String[] args) {
-        System.out.println("nvm we coo"); 
-    }
+        makeImage(30);
+        }        
+        static void makeImage(int scale){
+            makeLine(scale, true, false);
+            makeSurface(scale, scale);
+            makeLine(scale, false, true);
+            makeFace(scale);
+            makeLine(scale, false, false);
+        }
+        
+        static void makeLine(int x, boolean padding, boolean tail){
+            String line = "";
+            if(padding){
+            for(int i = 0; i<x; i += 1){
+                line += " ";
+                }
+            }
+            line += "+";
+            for(int i = 0; i<x; i += 1){
+                line += "---";
+            }
+            line += "+";
+            if(tail){
+            for(int i = 0; i<x; i+=1){
+                line += "/";
+            }
+            
+            }
+        System.out.println(line);
+        
+                    
+        }
+        
+        
+        
+        static void makeSurface(int x, int s){
+
+            for(int i = 1; i<s; i++){
+                String line = "";
+                for(int j = 1; j<x-i; j++){
+                    line += " ";
+                }
+                line += "/ ";
+                for(int j = 0; j<x-i; j++){
+                    line += "   ";
+                }
+                line += "_";
+                for(int j = 0; j<i; j++){
+                    line += "__/";
+                }
+                for(int j = 0; j<i; j++){
+                    line += "/";
+                }
+            System.out.println(line);
+            }
+        }
+    
+        static void makeFace(int x){
+            for(int i = 0; i<x/2; i++){
+                String line = "|";
+                for(int j=0; j < x; j++){
+                    line += " ";
+                }
+                //line += "|";
+                for(int j=0; j<x; j++){
+                    line += "  ";
+                }
+                line += "|";
+                
+                for(int j = 0; j<x-2*i; j+=1){
+                    line += "/";
+                }
+            
+            System.out.println(line);
+            }
+        }
+    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
