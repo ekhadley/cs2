94678b4a2fc523b40589618f53f5dde7950026c8
Ethan_Hadley <ekhadley@gmail.com>
2022-01-19 23:28:55 -0600

    dfgsfd

--- lab1/part4.java
+++ lab1/part4.java
@@ -12,32 +12,36 @@ public class part4 {
         static void makeRows(int l, int s){
             System.out.print("|");
             String day = "";
-            for(int i = 0; i<35; i++){
+
+            for(int i = 1-(s%7); i<35; i++){
                 day = "" + i;
-                if(day.length() > 1){
-                    System.out.print(" " + i + "  |");
+                if(i > 0 && i <= l){
+                    if(day.length() > 1){
+                        System.out.print("  " + i + "  |");
+                    }
+                    else{
+                        System.out.print("   " + i + "  |");
+                    }
                 }
                 else{
-                    System.out.print("  " + i + "  |");
-                if(i < 0 || i > l){
-                    System.out.print("     |");
-                }
-                }
-                if((i+1)%7 == 0 && i != 34 ){
+                    System.out.print("      |");
+                    }
+                
+                if((i+(s%7))%7 == 0 && i != 34){
                     System.out.print("\n|");
                 }
             }
             System.out.println();   
         }
 
-        static void makeCalendar(){    
+        static void makeCalendar(int l, int s){    
             System.out.println("  Sun    Mon    Tue    Wed    Thu    Fri    Sat");
             makeLine(7);
-            makeRows(31, 6);
+            makeRows(l, s);
             makeLine(7);
         }
 
     public static void main(String[] args) {
-        makeCalendar();
+        makeCalendar(31, 6);
     }
 }
