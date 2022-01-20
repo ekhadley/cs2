9b419fdfeace51e67e7243317e936918efc19bea
Ethan Hadley <ekhadley@gmail.com>
2022-01-20 10:41:35 -0600

    fdsgdfsdfsgds

--- lab2/part1.java
+++ lab2/part1.java
@@ -1,14 +1,35 @@
 package lab2;
+import java.util.Scanner;
+
 public class part1 {
-    static void askDay(){
+
+
+    static processDate(){
+        ArrayList<Integer> daysInMonth = new ArrayList<Integer>(Arrays.asList(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31));
+
+        var input = new Scanner(System.in);
+        System.out.println("Enter your birthdate (D/M/YYYY):");
+        String date = input.nextLine();
+        String[] dates = date.split("/");
+
+        int day = Integer.parseInt(dates[0]);
+        int month = Integer.parseInt(dates[1]);
+        int year = Integer.parseInt(dates[2]);
+
+        int yearDay = 0;
+        for(int i=0; i<month-1; i++){
+            yearDay += daysInMonth.get(i);
+        }
+        yearDay += day;
         
-    }
+        return yearDay;
+    } 
 
 
 
 
     public static void main(String[] args) {
-        //askDay("What is the first birthday? (M/D/YY)");
+        processDate("What is the first birthday?");
 
     }
 }
