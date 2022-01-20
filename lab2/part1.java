37e75d95a8d5568fc73dd37ab8fd6650dcd84e7c
Ethan Hadley <ekhadley@gmail.com>
2022-01-20 09:47:11 -0600

    dfgsdf

---
+++ lab2/part1.java
@@ -0,0 +1,14 @@
+package lab2;
+public class part1 {
+    static void askDay(){
+        
+    }
+
+
+
+
+    public static void main(String[] args) {
+        //askDay("What is the first birthday? (M/D/YY)");
+
+    }
+}
