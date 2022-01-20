0315dee5e8a8470d545cdc25917c9b38aec59999
Ethan_Hadley <ekhadley@gmail.com>
2022-01-19 19:18:32 -0600

    fghdfg

--- lab1/part3.java
+++ lab1/part3.java
@@ -3,17 +3,24 @@ import java.util.Scanner;
 
 public class part3 {
     public static void main(String[] args) {
-        
 
-            Scanner myObj = new Scanner(System.in);
-            System.out.println("Enter username");
+            var input = new Scanner(System.in);
+            System.out.println("Enter your name: ");
         
-            String userName = myObj.nextLine();
-            System.out.println("Username is: " + userName);
+            String name = input.nextLine();
+            String[] sep = name.split("\\s+");
+            
+            String reversed = "";
+            for(int i = sep.length-1; i>-1; i--){
+                reversed += sep[i];
+                if(sep.length > 1 && i > 0){
+                    reversed += ", ";
+                }
+            }
 
-    
-    
-    }
+            System.out.println("Your name in reverse order is: " + reversed);
+        }
+
 }
 
 
