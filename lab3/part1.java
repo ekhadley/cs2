0cfd00dc844519b143c6e691dfe398d1d1fdb03f
Ethan_Hadley <ekhadley@gmail.com>
2022-02-02 19:06:37 -0600

    gsdgd

--- lab3/part1.java
+++ lab3/part1.java
@@ -5,20 +5,78 @@ import java.util.*;
 
 public class part1 {
 
-    public static int wordCount(String filename){
-        File paper = new File(filename + ".txt");
-        Scanner scan = new Scanner(paper);
+    public static int[] wordCount(String filename) throws FileNotFoundException{
         String line;
+        String full = "";
+        int lineCount = 0;
+        int wordCount = 0;
+        int charCount = 0;
+        int[] counts = new int[3];
 
-        line = scan.nextLine();
-        System.out.println(line);
+        File paper = new File(filename);
+        Scanner scan = new Scanner(paper);
 
-        return 0;
+        while(scan.hasNext()){
+            line = scan.nextLine();
+            full += line;
+            lineCount ++;
+        }
+        String[] words = full.split("\\s+");
+
+        wordCount = words.length;
+        charCount = full.length();
+
+        counts[0] = lineCount;
+        counts[1] = wordCount;
+        counts[2] = charCount;
+
+        return counts;
     }
 
-    public static void main(String args[]){
-
-        System.out.print(wordCount("mypaper"));
+    public static void main(String args[]) throws FileNotFoundException{
+        int[] resp = wordCount("thePaper.txt");
+        System.out.println("This text file contains " + resp[0] + " lines, " + resp[1] + " words, and " + resp[2] + " lines");
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
