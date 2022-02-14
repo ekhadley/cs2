13b7dc90e2f40611003845a8296235992687411c
Ethan Hadley <ekhadley@gmail.com>
2022-02-14 09:49:48 -0600

    asdfas

--- lab5/Date.java
+++ lab5/Date.java
@@ -1,24 +1,48 @@
 package lab5;
+
 public class Date {
     int year;
     int month;
     int day;
     int[] daysPerMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    public Date(int year, int month, int day){
+    public Date(int month, int day, int year){
         this.year = year;
         this.month = month;
         this.day = day;
     }
 
+    public int yearlen(int year){
+        boolean type1 = this.year%400 == 0 && this.year%100 == 0;
+        boolean type2 = this.year%4 == 0 && this.year%100 != 0;
+        int yearlen = (type1 || type2)?366:365;
+        return yearlen;
+    }
+
     public void addDays(int inc){
-        this.day += inc;
-        int temp = day;
-        this.day -= daysPerMonth[this.month-1];
-        this.month += temp/daysPerMonth[this.month-1];
+        int absDate = this.day;
+        int yearlen = yearlen(this.year);
+
+        for(int i=0; i<this.month-1; i++){
+            absDate += daysPerMonth[i];
+        }
+        absDate += inc;
+
+        while(absDate>yearlen){
+            yearlen = yearlen(this.year);
+            absDate -= yearlen;
+            this.year++;
+            System.out.println(this.year + ", " + yearlen);
+        }
+        
+        for(int i = 0; absDate>daysPerMonth[i];i++){
+            absDate -= daysPerMonth[i];
+            this.month = i + 2;
+        }
+        this.day = absDate;
     }
 
     public String getDate(){
-        return "" + this.day + ", " + this.month + ", " + this.year;
+        return "" + this.month + ", " + this.day + ", " + this.year;
     }
 }
--- lab5/part2.java
+++ lab5/part2.java
@@ -2,8 +2,8 @@ package lab5;
 
 public class part2 {
     public static void main(String[] args){
-        Date yo = new Date(2022, 3, 26);
-        yo.addDays(100);
+        Date yo = new Date(3,26,2002);
+        yo.addDays(381);
         System.out.println(yo.getDate());
     }
 }
