9ff626f45afd374daee830e29d5cc4b8fa2dce5c
Ethan_Hadley <ekhadley@gmail.com>
2022-02-24 08:49:35 -0600

    fdghdf

--- lab6/animals.java
+++ lab6/animals.java
@@ -4,22 +4,36 @@ int h = 800;
 void setup(){
   background(30);
   size(w, h);
+  noStroke();
 }
   
 public class animal{
   PVector pos;
+  PVector acc;
   int size;
+  int hunger = 0;
+  int strength;
+  int speed;
+  color marker;
   
-  public animal(int posx, int posy, int size){
+  public animal(int posx, int posy, int size, color marker){
     this.pos = new PVector(posx, posy);
     this.size = size;
-    }
+    this.strength = 5;
+    this.speed = 10;
+    this.marker = marker;
+    Random random = new Random();
+  }
   
-
+  void move(){
+    if(this.hunger>0){this.hunger--;}
+    double newAcc = random.nextDouble();
+    
   
+  }
 }
   
-
+alice = Animal(400, 400, )
   
 void draw(){
   
