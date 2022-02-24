bc64d9c36301248d29d6c51e72303ddb5905dea0
Ethan_Hadley <ekhadley@gmail.com>
2022-02-24 08:27:06 -0600

    fdgs

---
+++ lab6/animals.java
@@ -0,0 +1,26 @@
+int w = 800;
+int h = 800;
+
+void setup(){
+  background(30);
+  size(w, h);
+}
+  
+public class animal{
+  PVector pos;
+  int size;
+  
+  public animal(int posx, int posy, int size){
+    this.pos = new PVector(posx, posy);
+    this.size = size;
+    }
+  
+
+  
+}
+  
+
+  
+void draw(){
+  
+}
