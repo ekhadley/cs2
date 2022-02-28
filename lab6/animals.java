fc69629638b89f86a2ee1c0fbca4442e19ed0983
Ethan_Hadley <ekhadley@gmail.com>
2022-02-28 07:38:16 -0600

    dfgsdf

--- lab6/animals.java
+++ lab6/animals.java
@@ -1,108 +1,197 @@
 import java.util.Random;
 import java.util.ArrayList;
 
-// This code was written to run inside of a visual/animation IDE for Java called Processing which
+// This code was written to run inside of a visual/animation IDE/library for Java called Processing which
 // I am familiar with so decided to use it for this assignment. The program has a tool to export a script as an 
-// executable but in my experience it rarely works. If it does not, you can donwload Processing 3.5.4 from:
-// processing.org/download
-// and run this on your own computer if you feel it necessary.
+// executable but in my experience it rarely works.  It is submited in the dropbox with the source code. If it does not work, 
+// you can donwload Processing 3.5.4 from processing.org/download
+// and run the code on your own computer if you feel it necessary.
 // sidenote, I think processing is an amazingly intuitive way to implement animation/interfaces for python or java,
 // and I would reccomend it for use in other classes.
 
 Random random = new Random();
 
-void setup(){
+
+public static PVector PVadd(PVector a, PVector b) {
+  return new PVector(a.x+b.x, a.y+b.y);
+}
+
+public static PVector PVscale(PVector a, float d) {
+  return new PVector(a.x*d, a.y*d);
+}
+
+public PVector oob(PVector pos){
+    if (pos.x > width) {
+      pos.x -= width;}
+    if (pos.x < 0) {
+      pos.x += width;}
+    if (pos.y > width) {
+      pos.y -= height;}
+    if (pos.y < 0) {
+      pos.y += height;}
+    return pos;
+}
+
+void setup() {
   background(30);
-  size(800, 800);
+  size(1000, 1000);
   noStroke();
 
-  for(int i = 0; i < 50; i++){
-    ecosystem.add(new dog(random.nextInt(width), 
-                             random.nextInt(height), 
-                             color(random.nextInt(100)+60, random.nextInt(300), random.nextInt(150)+80)));
+  for (int i = 0; i < 0; i++) {
+    ecosystem.add(new dog(random.nextInt(width), random.nextInt(height)));
+  }
+  for (int i = 0; i < 0; i++) {
+    ecosystem.add(new bird(random.nextInt(width), random.nextInt(height)));
+  }
+  for (int i = 0; i < 10; i++) {
+    ecosystem.add(new tiger(random.nextInt(width), random.nextInt(height)));
   }
 }
 
-
-public class animal{
+public class animal {
   PVector pos;
   PVector vel;
-  int size;
+  PVector acc;
+  float size;
   int hunger = 0;
   int strength;
   int speed;
   color marker;
+  int energy = 0;
 
-  
-  public animal(int posx, int posy, color marker){
-      this.pos = new PVector(posx, posy);
-      this.vel = new PVector(0, 0);
-      this.size = 5;
-      this.strength = 5;
-      this.speed = 10;
-      this.marker = marker;
+
+  public animal(float posx, float posy) {
+    this.pos = new PVector(posx, posy);
+    this.vel = new PVector(0, 0);
+    this.acc = new PVector(0, 0);
+    this.size = 5;
+    this.strength = 5;
+    this.speed = 10;
   }
-    
-      void move(){
-        if(this.hunger>0){this.hunger--;}
-        
-        this.vel.x += (random.nextDouble()-.5)*this.speed/10;
-        this.vel.y += (random.nextDouble()-.5)*this.speed/10;
-        
-        if(this.vel.mag()<1.5){
-            this.vel.x *= .95;
-            this.vel.y *= .95;
+
+  void spawn(float posx, float posy){
+    ecosystem.add(new animal(posx, posy));
+  }
+
+  void show(){
+    fill(this.marker);
+    ellipse(this.pos.x, this.pos.y, this.size, this.size);
+  }
+
+  void encounter(){
+    this.spawn(this.pos.x, this.pos.y);
+    /*
+    if(this.energy>0){
+      for(int i=0; i<ecosystem.size(); i++){
+        if(this.getClass() == ecosystem.get(i).getClass() && this.energy > 100 && ecosystem.get(i).energy > 100){
+           this.spawn(this.pos.x, this.pos.y);
         }
-        
-        this.pos.x += vel.x;
-        this.pos.y += vel.y;
- 
-        if(this.pos.x > width){this.pos.x -= width;}
-        if(this.pos.x < 0){this.pos.x += width;}
-        if(this.pos.y > width){this.pos.y -= height;}
-        if(this.pos.y < 0){this.pos.y += height;}
       }
-      void show(){
-        fill(this.marker);
-        ellipse(this.pos.x, this.pos.y, this.size, this.size);
-      }
-    
+    }
+    */
+  }
+
+  void update(){
+      this.encounter();
+      this.move();
+      this.show();
+  }
+
+  void move() {
+    //this.energy--;
+    this.acc = PVadd(this.acc, new PVector((random.nextFloat()-.5)/5, (random.nextFloat()-.5)/5));
+    this.vel = PVadd(this.vel, this.acc);
+    if (this.vel.mag()>this.speed/10.0){
+      this.vel = PVscale(this.vel, .4);
+      this.acc = PVscale(this.vel, .5);}
+    this.pos = PVadd(this.pos, this.vel);
+    this.pos = oob(this.pos);
+  }
 }
-public class dog extends animal{
-  public dog(int posx, int posy, color marker){
-    super(posx, posy, marker);
+
+public class dog extends animal {
+  public dog(float posx, float posy) {
+    super(posx, posy);
     this.size = 17;
     this.strength = 7;
-    this.speed = 6;
+    this.speed = 10;
+    this.marker = color(random.nextInt(150)+50, random.nextInt(30), random.nextInt(150)+100);
   }
-  
-  void show(){
+
+  void spawn(float posx, float posy){
+    ecosystem.add(new dog(posx, posy));
+  }
+
+  void show() {
     resetMatrix();
     fill(this.marker);
     translate(this.pos.x, this.pos.y);
     rotate(this.vel.heading()-PI/2);
     triangle(-this.size/2, 0, this.size/2, 0, 0, this.size);
     triangle(-this.size/2, 1, this.size/2, 1, 0, -this.size/3);
-
   }
 }
 
+public class bird extends animal {
+  public bird(float posx, float posy) {
+    super(posx, posy);
+    this.size = 5.5;
+    this.strength = 3;
+    this.speed = 30;
+    this.marker = color(random.nextInt(50), random.nextInt(150)+100, random.nextInt(100)+80);
+    }
+
+  void spawn(float posx, float posy){
+    ecosystem.add(new bird(posx, posy));
+  }
+
+  void show() {
+    resetMatrix();
+    fill(this.marker);
+    translate(this.pos.x, this.pos.y);
+    rotate(this.vel.heading()-PI/2);
+    triangle(-this.size, -this.size, 1, 0, 0, this.size*2);
+    triangle(this.size, -this.size, -1, 0, 0, this.size*2);
+  }
+}
+public class tiger extends animal {
+  public tiger(float posx, float posy) {
+    super(posx, posy);
+    this.size = 20;
+    this.strength = 30;
+    this.speed = 30;
+    this.marker = color(random.nextInt(100)+150, random.nextInt(140)+90, random.nextInt(90));
+  }
+
+  void spawn(float posx, float posy){
+    ecosystem.add(new tiger(posx, posy));
+  }
+
+  void show() {
+    resetMatrix();
+    fill(this.marker);
+    translate(this.pos.x, this.pos.y);
+    rotate(this.vel.heading()-PI/2);
+    triangle(-this.size/2, 0, this.size/2, 0, 0, this.size);
+    triangle(-this.size/2, 1, this.size/2, 1, 0, -this.size);
+  }
+}
 
 ArrayList<animal> ecosystem = new ArrayList<animal>();
 
-void draw(){
+int i = 0;
+void draw() {
   background(30);
+
+  System.out.println(i);
+  i++;
   
-  for(animal i : ecosystem){
-    dog j = (dog) i;
-    j.move();
-    j.show();
-
-
+  for (animal i : ecosystem) {
+    if(i instanceof bird){bird j = (bird)i; j.update();}
+    if(i instanceof dog){dog j = (dog)i;j.update();}
+    if(i instanceof tiger){tiger j = (tiger)i;j.update();}
   }
-  
 }
-
 
 
 
