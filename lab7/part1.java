5e3d8d451a94e54b55ce2f39bcfc706bfe858704
Ethan Hadley <ekhadley@gmail.com>
2022-03-04 09:48:11 -0600

    gfhdghdfghd

--- lab7/part1.java
+++ lab7/part1.java
@@ -1,13 +1,18 @@
 package lab7;
+import java.util.*;
 
 public class part1 {
-    static ArrayList filterRange(ArrayList a, int l, int u){
+    static ArrayList<Integer> filterRange(ArrayList<Integer> a, int l, int u){
+        ArrayList<Integer> b = new ArrayList<Integer>();
+        b.add(a);
         for(int i : a){
-            if(i > l && i < u){a.remove(i);}
+            if(i > l && i < u){b.add(i);}
         }
+        a.removeAll(b);
         return a;
     }
     public static void main(String[] args){
-        
+        ArrayList<Integer> l = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 354));
+        System.out.println(filterRange(l, 3, 8));
     }
 }
