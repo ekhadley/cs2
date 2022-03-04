11b4a7db0a6e8a91e02da24196b36bc34d73d826
Ethan Hadley <ekhadley@gmail.com>
2022-03-04 09:51:30 -0600

    sfdgsd

--- lab7/part1.java
+++ lab7/part1.java
@@ -3,16 +3,18 @@ import java.util.*;
 
 public class part1 {
     static ArrayList<Integer> filterRange(ArrayList<Integer> a, int l, int u){
-        ArrayList<Integer> b = new ArrayList<Integer>();
+        ArrayList b = new ArrayList();
         b.add(a);
         for(int i : a){
-            if(i > l && i < u){b.add(i);}
+            if(i >= l && i <= u){b.add(i);}
         }
         a.removeAll(b);
         return a;
     }
     public static void main(String[] args){
-        ArrayList<Integer> l = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 354));
-        System.out.println(filterRange(l, 3, 8));
+        ArrayList<Integer> l1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 354));
+        ArrayList<Integer> l2 = new ArrayList<Integer>(Arrays.asList(4, 2, 54, 7, 5, 8, 9, 99, 23));
+        ArrayList<Integer> l3 = new ArrayList<Integer>(Arrays.asList(0, 1, 1, 1, 0, 0, 0, 1, 1, 1));
+        System.out.println(filterRange(l1, 3, 8));
     }
 }
