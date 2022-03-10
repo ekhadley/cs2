eb113e3383fcc9c9cecec9da7623df06fe6a5d5c
Ethan_Hadley <ekhadley@gmail.com>
2022-03-10 08:00:54 -0600

    sdfgsd

--- lab7/part2.java
+++ lab7/part2.java
@@ -4,9 +4,7 @@ public class part2 {
     static ArrayList<String> clump(ArrayList<String> a) throws IndexOutOfBoundsException{
         ArrayList<String> c = new ArrayList<>();
         for(int i = 0; i < a.size(); i+=2){
-            if(i+1 < a.size()){
                 c.add("(" + a.get(i) + " " + a.get(i+1) + ")");
-            }
         }
         return c;
     }
