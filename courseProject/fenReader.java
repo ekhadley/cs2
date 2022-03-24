ea2c86bf8cfaf14055952b97824144b02b850aa9
Ethan Hadley <ekhadley@gmail.com>
2022-03-24 10:32:07 -0500

    dfsghdsf

--- courseProject/fenReader.java
+++ courseProject/fenReader.java
@@ -1,16 +1,99 @@
+import java.util.*;
+
 void setup(){
     size(800, 800);
-    background(#FFBD8B);
+    background(#A26A3E);
     noStroke();
-    
+    imageMode(CENTER);
+
+    p = loadImage("pieces.png");
+    tile();
+}
+  
+void tile(){
+    background(#A26A3E);
     for(int i=0; i<32; i++){
-      fill(#A26A3E);
+      fill(#FFBD8B);
       translate((2*(width/8)*i)%(width) + ((i/4)%2)*(width/8), (width/8)*(i/4));
       rect(0, 0, width/8, width/8);
       resetMatrix();
     }
+}
+
+public PVector getScreenPos(String place){
+  String[] cols = {"A", "B", "C", "D", "E", "F", "G", "H"};
+  String[] inp = place.split("");
+  int x = 0;
+  for(int i = 0; i<8; i++){
+    if(inp[0].matches(cols[i])){
+      x = 100*i + width/16;
+    }  
   }
-  void place(String piece, String place){
-    
+  int y = (8-Integer.parseInt(inp[1]))*100 + height/16;
+  return new PVector(x, y);
+}
+
+public PVector rowCol(String place){
+  String[] cols = {"A", "B", "C", "D", "E", "F", "G", "H"};
+  String[] inp = place.split("");
+  int x = 0;
+  for(int i = 0; i<8; i++){
+    if(inp[0].matches(cols[i])){
+      x = i;
+    }  
+  }
+  int y = Integer.parseInt(inp[1])-1;
+  return new PVector(x, y);
+}
+
+class bishop{
+  PVector pos;
   
+  public bishop(int c, int r, String colour){
+    this.pos = new PVector(c, r);
   }
+  public boolean canMove(PVector d){
+    if(this.pos.x-d.x == this.pos.y-)
+  }
+}
+
+
+PImage p;
+
+
+void draw(){
+  tile();
+
+ 
+  image(p, 400, 400);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
