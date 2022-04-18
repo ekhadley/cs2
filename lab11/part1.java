84975074826aea3a7778cc684ee0d65cc667b779
Ethan Hadley <ekhadley@gmail.com>
2022-04-18 09:47:24 -0500

    ghfjf

---
+++ lab11/part1.java
@@ -0,0 +1,10 @@
+package lab11;
+import java.util.*
+
+public class part1 {
+    
+    public static void main(String[] args]){
+        
+    }
+ 
+}
---
+++ lab11/part2.java
@@ -0,0 +1,15 @@
+package lab11;
+import java.util.*;
+
+public class part2 {
+    public Stack<int> collapse(Stack s){
+        ArrayList<int> c = new Arraylist<int>
+        while(s.length>1){
+            c.add
+        }
+    }
+    public static void main(String[] args){
+        Stack<int> s = new Stack<>(Arrays.asList7, 2, 8, 9, 4, 11, 7, 1, 42));
+
+    }
+}
