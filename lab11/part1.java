3ea533877d10687e823d3bc9120618f80d399d88
Ethan Hadley <ekhadley@gmail.com>
2022-04-20 09:17:03 -0500

    sdfgsd

--- lab11/part1.java
+++ lab11/part1.java
@@ -1,10 +1,25 @@
 package lab11;
-import java.util.*
+import java.util.*;
 
 public class part1 {
-    
-    public static void main(String[] args]){
-        
+    public Arraylist<int> collapse(Stack s){
+        ArrayList<int> c = new Arraylist<int>
+        int n = 0;
+        while(s.length>1){
+            n += s.peek();
+            s.pop();
+            n += s.peek();
+            s.pop();
+            c.add(0, n);
+            n = 0;
+        }
+        if(s.length == 1){
+            c.add(0, s.peek())
+        }
+        return c;
     }
- 
+    public static void main(String[] args){
+        Stack<int> s = new Stack<>(Arrays.asList7, 2, 8, 9, 4, 11, 7, 1, 42));
+        System.out.println(Arrays.toString(collapse(s)));
+    }
 }
--- lab11/part2.java
+++ lab11/part2.java
@@ -1,15 +1,10 @@
 package lab11;
-import java.util.*;
+import java.util.*
 
-public class part2 {
-    public Stack<int> collapse(Stack s){
-        ArrayList<int> c = new Arraylist<int>
-        while(s.length>1){
-            c.add
-        }
+public class part1 {
+    
+    public static void main(String[] args]){
+        
     }
-    public static void main(String[] args){
-        Stack<int> s = new Stack<>(Arrays.asList7, 2, 8, 9, 4, 11, 7, 1, 42));
-
-    }
+ 
 }
